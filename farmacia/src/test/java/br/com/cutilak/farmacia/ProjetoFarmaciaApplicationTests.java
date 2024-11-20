dbc407f6badf45715d24822d2a1f7526accfe2e9
Luis Fernando Cutilak <luiscutilak@gmail.com>
2024-11-20 12:28:39 -0300

    Initializing Project

---
+++ farmacia/src/test/java/br/com/cutilak/farmacia/ProjetoFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cutilak.farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
